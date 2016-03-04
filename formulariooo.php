624515e7ceaeb099ce703393076470834856c084
lupitaaa <sabjg.95@gmail.com>
2016-03-03 22:10:47 -0600

    practica 5

---
+++ formulariooo.php
@@ -0,0 +1,15 @@
+<html>
+ <body>
+  <form action="saludo.php"
+     method="POST">
+    <label> Usuario </label>
+    <input type="text"
+      name="usuario"></br>
+    <label> Password </label>
+    <input type="password"
+      name="pass"></br>
+    <input type="submit"
+      value="enviar">
+  </form>
+ </body>
+</html> 
---
+++ saludo.php
@@ -0,0 +1,10 @@
+<?php
+ $usuario=$_POST["usuario"];
+ $pass=$_POST["pass"];
+
+ if ($usuario=='root'&& $pass=='1234'){ 
+   echo "Hola Bienvenido ";}
+
+ else echo"Hola, gracias por visitarnos";
+      
+?>
